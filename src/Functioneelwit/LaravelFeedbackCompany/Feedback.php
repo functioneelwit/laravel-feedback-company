f8c98e1131aac16ffc67db867eaaa0ba88cbf480
Mattijs Wit <mattijs@functioneelwit.nl>
2017-03-16 14:55:40 +0100

    curl setup, configuration

--- src/Functioneelwit/LaravelFeedbackCompany/Feedback.php
+++ src/Functioneelwit/LaravelFeedbackCompany/Feedback.php
@@ -7,11 +7,40 @@ class Feedback {
  
     public function send($email){
 
+    	if(! Config::has('laravel-feedback-company::connector'))
+    	{
+    		return 'Can\'t find config file. Please run: php artisan config:publish functioneelwit/laravel-feedback-company';
+    	} 
+
     	$connector = Config::get('laravel-feedback-company::connector');
+    	$uri = Config::get('laravel-feedback-company::uri');
+
+    	if(Config::has('laravel-feedback-company::delay'))
+    	{
+    		$delay = Config::get('laravel-feedback-company::delay');
+
+    		if(! $delay)
+    		{
+    			unset($delay);
+    		}
+    	}
+
+    	if(Config::has('laravel-feedback-company::remindDelay'))
+    	{
+    		$remindDelay = Config::get('laravel-feedback-company::remindDelay');
+
+    		if(! $remindDelay)
+    		{
+    			unset($remindDelay);
+    		}
+    	}
+    	
+    	// remove spacing, linebreaks & lowercase emailaddress
+    	$email = strtolower(preg_replace("/\s*/m", "", $email));
 
     	if(! $connector)
     	{
-    		return 'No FeedbackCompany connector set. Please run: php artisan config:publish functioneelwit/laravel-feedback-company';
+    		return 'No connector setting in config.';
     	}
 
     	if (filter_var($email, FILTER_VALIDATE_EMAIL) === false)
@@ -19,16 +48,42 @@ class Feedback {
     		return 'Not a valid e-mailaddress.';
     	}
 
-    	$url = 'https://connect.feedbackcompany.nl/feedback/?
+    	$url = $uri . '?
 			action=sendInvitation
 			&connector=' . $connector . '
 			&user=' . $email . '
-			&delay=2
-			&remindDelay=4';
+			&Chksum=' . $this->checksum($email);
+
+		if(isset($delay))
+		{
+			$url .= '&delay=' . $delay;
+		}
+
+		if(isset($remindDelay))
+		{
+			$url .= '&remindDelay=' . $remindDelay;
+		}
 
 		$url = preg_replace("/\s*/m", "", $url);
 
-		return $url;
+		$curl = curl_init();
+
+		curl_setopt_array($curl, [
+			CURLOPT_RETURNTRANSFER => 1,
+    		CURLOPT_URL => $url
+		]);
+		
+		$result = curl_exec($curl);
+
+		var_dump($result);
+		
+		curl_close($curl);
+		
+    }
+
+    private function checksum($string)
+    {
+    	return array_sum(array_map('ord', str_split($string)));
     }
  
 }
--- src/config/config.php
+++ src/config/config.php
@@ -2,6 +2,16 @@
 
 return array(
 
+	// Uri for the get request for The Feedback Company (https://feedbackcompany.nl/)
+	'uri' => 'https://connect.feedbackcompany.nl/feedback/',
+	
+	// Unique identifier provided by The Feedback Company
 	'connector' => '',
+
+	// Number of days whereafter the review invite is send. Defaults to 7.
+	'delay' => 0,
+
+	// Number of days whereafter a reminder is send. Defaults to 0 (no reminder).
+	'remindDelay' => 0,
 	
 );
